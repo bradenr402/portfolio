0f7dda5d02d2f0d7a3f7d77529e49ffaf27b6c4d
bradenr402 <bradenr402@gmail.com>
2024-11-19 13:07:48 -0500

    setup project structure, webpack, and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  darkMode: 'class',
+  mode: 'jit',
+  content: ['./dist/*.html'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};

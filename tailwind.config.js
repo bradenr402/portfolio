54bb3766cbdac30b5ae7e124e2fd39f0ac2e0fd2
bradenr402 <bradenr402@gmail.com>
2024-11-20 13:04:50 -0500

    feat: add primary colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ module.exports = {
   mode: 'jit',
   content: ['./dist/*.html', './src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Calibre', 'sans-serif'],
+      },
+      colors: {
+        primary: {
+          50: '#fde8e7',
+          100: '#fbd0ce',
+          200: '#f7a19d',
+          300: '#f3726c',
+          400: '#ef433b',
+          500: '#c1281c',
+          600: '#9a2017',
+          700: '#731812',
+          800: '#4c100c',
+          900: '#250806',
+        },
+      },
+    },
   },
   variants: {
     extend: {},

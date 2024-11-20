242fdeee4c4d56aed817fe4251ae037a107d70bd
bradenr402 <bradenr402@gmail.com>
2024-11-20 15:16:22 -0500

    feat: use 'html-loader' for nav component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.css';
+import nav from './components/nav.html';
+
+const parseHTML = (html) => new DOMParser().parseFromString(html, 'text/html').body.firstChild;
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('nav').replaceWith(parseHTML(nav));
+});
--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,10 @@ module.exports = {
         include: path.resolve(__dirname, 'src'),
         use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
+      {
+        test: /\.html$/i,
+        use: ['html-loader'],
+      },
     ],
   },
 };

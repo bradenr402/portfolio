e526a38aa7803a0e032c04613394dc9dcdeeb17d
bradenr402 <bradenr402@gmail.com>
2024-12-02 08:49:44 -0500

    bug fixes

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
+  mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',

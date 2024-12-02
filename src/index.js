1db789fd456a53f2b0ce48d8525dc624b2451aea
bradenr402 <bradenr402@gmail.com>
2024-12-02 10:08:25 -0500

    refactor: remove console debugging statements

--- src/index.js
+++ src/index.js
@@ -16,25 +16,19 @@ const addFooter = () => {
 };
 
 const activateCurrentNavLink = () => {
-  console.log('Activating current nav link...');
   const links = document.querySelectorAll('a.nav-link-text');
-  console.log('Found nav links:', links.length);
   const currentPath = window.location.pathname.replace(/\.html$/, '');
-  console.log('Current path:', currentPath);
 
   links.forEach((link) => {
     const linkPath = new URL(link.href).pathname.replace(/\.html$/, '');
-    console.log('Checking link path:', linkPath);
 
     if (linkPath === currentPath) {
-      console.log('Match found - activating link:', linkPath);
       link.classList.add('nav-link-text-active');
       link.nextElementSibling.classList.add('nav-link-bg-active');
     } else if (
       (currentPath === '/' || currentPath === '') &&
       (linkPath === '/index.html' || linkPath === '/index')
     ) {
-      console.log('Index page match found - activating link:', linkPath);
       link.classList.add('nav-link-text-active');
       link.nextElementSibling.classList.add('nav-link-bg-active');
     }

48290c595627221d6f38f7ad41ffb3bec4d0d84b
Braden Roth <bradenr402@gmail.com>
2025-03-07 15:15:43 -0500

    refactor: remove comments from `src/setup-icons.js`

--- src/setup-icons.js
+++ src/setup-icons.js
@@ -4,18 +4,16 @@ const requireIcons = require.context('./images/icons', false, /\.svg$/);
 
 export default function setupIcons() {
   document.querySelectorAll('[data-icon]').forEach((element) => {
-    const iconName = element.getAttribute('data-icon'); // Get the icon name from the data attribute
+    const iconName = element.getAttribute('data-icon');
 
-    // Dynamically import the SVG based on the icon name
     const iconFile = requireIcons(`./${iconName}.svg`);
 
     if (iconFile) {
-      const svgElement = parseHTML(iconFile); // Parse the imported SVG content
+      const svgElement = parseHTML(iconFile);
 
-      // Copy classes from the div to the SVG element
+      // Copy Tailwind classes from the div to the SVG element
       svgElement.classList.add(...element.classList);
 
-      // Replace the div with the SVG element
       element.replaceWith(svgElement);
     } else {
       console.error(`SVG icon "${iconName}" not found`);

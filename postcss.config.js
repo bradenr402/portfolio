7c5a72d69d5f58776feb87685fffc7210d719092
Braden Roth <bradenr402@gmail.com>
2025-05-26 16:38:56 -0400

    config: bump tailwind to v4

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,5 @@
-/* eslint-disable import/no-extraneous-dependencies */
-const tailwindcss = require('tailwindcss');
-const autoprefixer = require('autoprefixer');
-
-module.exports = {
-  plugins: [
-    'postcss-preset-env',
-    tailwindcss,
-    autoprefixer,
-  ],
+export default {
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
 };

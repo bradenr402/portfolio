fad99d1ac7e6c1e65926fbbfdc1f3a9362bb6fe7
Braden Roth <bradenr402@gmail.com>
2026-01-10 16:17:03 -0500

    fix: add safeguard against non-existent icons

--- src/setup-icons.js
+++ src/setup-icons.js
@@ -3,18 +3,22 @@ import parseHTML from './parse-html';
 const requireIcons = require.context('./images/icons', false, /\.svg$/);
 
 export default function setupIcons() {
-  document.querySelectorAll('[data-icon]').forEach((element) => {
-    const iconName = element.getAttribute('data-icon');
+  for (const element of document.querySelectorAll('[data-icon]')) {
+    const iconName = element.dataset.icon;
 
-    const iconFile = requireIcons(`./${iconName}.svg`);
-
-    if (iconFile) {
-      const svgElement = parseHTML(iconFile);
-
-      // Copy Tailwind classes from the div to the SVG element
-      svgElement.classList.add(...element.classList);
-
-      element.replaceWith(svgElement);
+    let iconFile;
+    try {
+      iconFile = requireIcons(`./${iconName}.svg`);
+    } catch (error) {
+      // Icon not found; safely skip this element
+      continue;
     }
-  });
+
+    const svgElement = parseHTML(iconFile);
+
+    // Copy Tailwind classes from the div to the SVG element
+    svgElement.classList.add(...element.classList);
+
+    element.replaceWith(svgElement);
+  }
 }

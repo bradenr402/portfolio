bf0531baf28b08a27024ec56adf2d807a5516998
Braden Roth <bradenr402@gmail.com>
2026-08-06 17:53:59 -0400

    Fix non-global regex in `textToSlug` utility

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -12,7 +12,7 @@ export function textToSlug(text) {
     .toLowerCase()
     .trim()
     .replace(/\s*&\s*/g, '--')
-    .replace(/\./, '-')
+    .replace(/\./g, '-')
     .replace(/[^\w\s-]/g, '')
     .replace(/\s+/g, '-')
     .replace(/^-+|-+$/g, '');
